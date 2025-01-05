28a4d1ab86fc6dc2e0eda6a6b9a30241f15738ec
Kundalini Simp <arcthevox@gmail.com>
2025-01-05 22:13:32 +0100

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,5 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
-// Console.Write("$ ");
-
-// Wait for user input
+Console.Write("$ ");
 Console.ReadLine();
