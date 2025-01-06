790c62c51cb2db981120d3df0e8825382c27cad2
Kundalini Simp <arcthevox@gmail.com>
2025-01-06 16:56:53 +0100

    [any message]

--- src/PathManager.cs
+++ src/PathManager.cs
@@ -18,9 +18,7 @@ namespace CodeCraftersShell
             foreach (string dir in directories) {
                 string fullPath = dir + ShellConstants.ENV_DIR_SEPARATOR + exeName;
 
-                Console.WriteLine(fullPath);
-
-                if (File.Exists(fullPath + ShellConstants.EXT_EXE)) {
+                if (File.Exists(fullPath)) {
                     return fullPath;
                 }
             }
