c2dd553a4edda42f8ecc2e943ee2e961df6350cb
Kundalini Simp <arcthevox@gmail.com>
2025-05-25 11:57:45 +0200

    *

--- src/Shell.cs
+++ src/Shell.cs
@@ -67,7 +67,7 @@ namespace CodeCraftersShell
                 case ShellConstants.CMD_TYPE: CmdType(arguments, response); break;
                 case ShellConstants.CMD_PWD: CmdPwd(response); break;
                 case ShellConstants.CMD_CD: CmdCd(arguments, response); break;
-                case ShellConstants.CMD_CAT: CmdCat(arguments, response); break;
+                //case ShellConstants.CMD_CAT: CmdCat(arguments, response); break;
                 //case ShellConstants.CMD_LS: CmdLs(arguments, response); break;
                 case ShellConstants.CMD_CLEAR: CmdClear(); break;
                 case ShellConstants.CMD_EXIT: CmdExit(); break;
